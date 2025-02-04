e581004b46e9f1684358f8667f0b094fb434a98d
Joni Mesfin <jonimsfn@gmail.com>
2025-02-04 18:00:57 +0300

    view user form started

---
+++ stephen_grider/ch07_web_framework/src/View/UserForm.ts
@@ -0,0 +1,11 @@
+export class UserForm {
+  public parent: Element;
+  public template(): string{
+    return `
+    <div>
+    <h1>User Form</h1>
+    </div>
+    `
+  };
+  public render(): void;
+}
